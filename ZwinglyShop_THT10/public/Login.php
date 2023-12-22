332d7162d2987fff06713499db66adaaeeca4fe2
Zwinglyy <107592207+Zwinglyy@users.noreply.github.com>
2023-12-22 15:06:39 +0700

    THT10

---
+++ ZwinglyShop_THT10/public/Login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get username and password from the form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Simulate a database (in-memory array)
+    $registeredUsers = isset($_SESSION['registeredUsers']) ? $_SESSION['registeredUsers'] : [];
+
+    // Check if the username and password match
+    if ($username==='chris' && $password === '123') {
+        // Redirect to main.html on successful login
+        header("Location: Main");
+        exit();
+    } else {
+        echo "Invalid username or password. Please try again.";
+    }
+}
+?>
